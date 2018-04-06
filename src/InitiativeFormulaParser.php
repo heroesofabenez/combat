f2d70f86b0d2b2ebf3481f0fe6c2832e08318023
Jakub Konečný <jakub.konecny2@centrum.cz>
2018-04-06 13:49:44 +0200

    use preg_match() instead of preg_match_all() in InitiativeFormulaParser::calculateInitiative()

--- src/InitiativeFormulaParser.php
+++ src/InitiativeFormulaParser.php
@@ -18,12 +18,12 @@ final class InitiativeFormulaParser implements IInitiativeFormulaParser {
       "CHAR" => $character->charisma,
     ];
     $formula = str_replace(array_keys($stats), array_values($stats), $formula);
-    preg_match_all("/^([1-9]+)d([1-9]+)/", $formula, $dices);
-    for($i = 1; $i <= (int) $dices[1][0]; $i++) {
-      $result += rand(1, (int) $dices[2][0]);
+    preg_match("/^([1-9]+)d([1-9]+)/", $formula, $dices);
+    for($i = 1; $i <= (int) $dices[1]; $i++) {
+      $result += rand(1, (int) $dices[2]);
     }
-    preg_match_all("/\+([0-9]+)\/([0-9]+)/", $formula, $ammendum);
-    $result += (int) $ammendum[1][0] / (int) $ammendum[2][0];
+    preg_match("/\+([0-9]+)\/([0-9]+)/", $formula, $ammendum);
+    $result += (int) $ammendum[1] / (int) $ammendum[2];
     return (int) $result;
   }  
 }
