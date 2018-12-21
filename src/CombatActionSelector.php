aab39460f1fbec93cce35c4049d93adcc77c3ce0
Jakub Konečný <jakub.konecny2@centrum.cz>
2018-12-21 19:43:51 +0100

    removed property CombatActionSelector::$defaultAction this practically reverts 033bf5a

--- src/CombatActionSelector.php
+++ src/CombatActionSelector.php
@@ -9,27 +9,9 @@ use HeroesofAbenez\Combat\CombatActions\ICombatAction;
  * CombatActionSelector
  *
  * @author Jakub Konečný
- * @property string $defaultAction
  */
 final class CombatActionSelector implements ICombatActionSelector {
   use \Nette\SmartObject;
-
-  /** @var string */
-  protected $defaultAction = CombatLogEntry::ACTION_ATTACK;
-
-  /**
-   * @return string
-   */
-  public function getDefaultAction(): string {
-    return $this->defaultAction;
-  }
-
-  /**
-   * @param string $defaultAction
-   */
-  public function setDefaultAction(string $defaultAction): void {
-    $this->defaultAction = $defaultAction;
-  }
 
   public function chooseAction(CombatBase $combat, Character $character): ?string {
     if($character->hitpoints < 1) {
@@ -45,7 +27,7 @@ final class CombatActionSelector implements ICombatActionSelector {
         return $action->getName();
       }
     }
-    return $this->defaultAction;
+    return null;
   }
 }
 ?>
